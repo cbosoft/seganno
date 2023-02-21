b91890392f68787b7cfeb2a1d2c8b325626a3236
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-21 14:45:56 +0000

    Add resnet featuredet as augmentation

--- annot/image_aug.py
+++ annot/image_aug.py
@@ -1,9 +1,10 @@
 from typing import Optional, List
 
 import numpy as np
+import cv2
 import scipy.ndimage
 
-from PySide6.QtWidgets import QGroupBox, QFormLayout, QSlider, QCheckBox, QPushButton
+from PySide6.QtWidgets import QGroupBox, QFormLayout, QSlider, QCheckBox, QPushButton, QComboBox
 from PySide6.QtCore import Qt
 
 
@@ -133,6 +134,61 @@ class EdgeDet(Filter2D_Aug):
         ]), 'Edge Detection')
 
 
+class ResNetFeatDet(Augmentation):
+
+    def __init__(self):
+        super().__init__('ResNet (ImageNet)')
+        from torchvision.models import resnet18, ResNet18_Weights
+        self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.model.eval()
+        self.w = None
+    
+    def apply_to_image(self, image: np.ndarray):
+        image[:] = self.inference(image, self.w.currentIndex())
+    
+    def inference(self, image: np.ndarray, level: int):
+
+        if not level:
+            return image
+        
+        h, w = image.shape
+        if len(image.shape) < 3:
+            image = np.stack([image, image, image], -1)
+
+        import torch
+        x = torch.tensor(image.astype(float)/255.).permute(2, 0, 1).float().unsqueeze(0)
+        with torch.no_grad():
+            x = self.model.conv1(x)
+            x = self.model.bn1(x)
+            x = self.model.relu(x)
+            x = self.model.maxpool(x)
+
+            x = self.model.layer1(x)
+
+            if level >= 2:
+                x = self.model.layer2(x)
+
+                if level >= 3:
+                    x = self.model.layer3(x)
+            
+                    if level >= 4:
+                        x = self.model.layer4(x)
+            
+            x = torch.mean(x, 1)
+            x = (np.array(x[0])*255.).astype(np.uint8)
+        return cv2.resize(x, (w, h))
+    
+    def widget(self, update_f):
+        self.w = QComboBox()
+        self.w.addItems([str(i) for i in range(5)])
+        self.w.setCurrentIndex(0)
+        self.w.currentIndexChanged.connect(update_f)
+        return self.w
+    
+    def reset(self):
+        self.w.setValue(0)
+
+
 class AugmentationToolbox(QGroupBox):
 
     def __init__(self, app):
@@ -145,6 +201,7 @@ class AugmentationToolbox(QGroupBox):
             ContrastAdjust(),
             Smooth(),
             EdgeDet(),
+            ResNetFeatDet(),
         ]
 
         self.augmentations = ComposedAugmentations(augs)
